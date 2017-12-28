f633af0cee76abded4ea50ec56e08f820427019d
nevermoi <wlgq@hotmail.com>
2017-12-28 18:03:47 +0800

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  title = 'Diet Helper';
+  isStarted: boolean = false;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  step1Form = {
+    fullName: 'o',
+    email: '',
+    age: null,
+    gender: 'female'
+  }
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      nameCtrl: ['', Validators.required],
+      emailCtrl: ['', Validators.required],
+      ageCtrl: [null, Validators.max(70)]
+    });
+
+    this.secondFormGroup = this._formBuilder.group({
+      nameCtrl: ['', Validators.required]
+    });
+  }
+
+  public report () {
+    console.log(this.step1Form);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { UniversalMaterialModule } from './universal-material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UniversalMaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
